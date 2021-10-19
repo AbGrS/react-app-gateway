d7b004a7e30e03534ba59438a82d683c15c9489f
Rahul Dwivedi <rahul.dwivedi@synapse.com>
2021-10-19 18:30:51 +0530

    adding home page in package json

--- my-app/src/redux/actions/loginApis.js
+++ my-app/src/redux/actions/loginApis.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 // To navigate around CORS error, I hosted a proxy on Heroku server
+// Ideally the backened must allow the cross origin requests by explicitly setting the header ("Access-Control-Allow-Origin": *)
 const PROXY = `https://sheltered-mountain-74311.herokuapp.com/`;
 const BASE_URL =  `http://35.207.169.147/`;
 function addProxy(url){
